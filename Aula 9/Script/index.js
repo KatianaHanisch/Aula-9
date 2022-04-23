8fdfbfacb9aff4946c29599b91cf85a46b0ca182
KatianaHanisch <101157580+KatianaHanisch@users.noreply.github.com>
2022-04-23 17:35:21 -0400

    Add files via upload

---
+++ Aula 9/Script/index.js
@@ -0,0 +1,12 @@
+$.support.cors = true;
+
+$('#btn').click(
+    function(){
+        $('main').load("../Lista/lista.html")
+    }
+)
+$('#btn2').click(
+    function(){
+        $('main').load("../Cadastro/cadastro.html")
+    }
+)
